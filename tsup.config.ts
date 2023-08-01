029f186505f34709c8b0140c775778a18edcd173
firefly <enfirefly@gmail.com>
2023-08-01 15:18:41 +0800

    perf: use tsup

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/extension.ts'],
+  format: ['cjs'],
+  shims: false,
+  dts: false,
+  clean: true,
+  env: {
+    NODE_ENV: process.env.NODE_ENV || 'production',
+  },
+  external: [
+    'vscode',
+  ],
+})
